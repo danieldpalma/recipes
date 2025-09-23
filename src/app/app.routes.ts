bc5a1f1da54ab4f17a29dde1140cbbc1f8b0abbe
danieldpalma <danielpalma232@gmail.com>
2025-09-23 11:10:12 -0300

    added lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,12 @@ import { RecipesComponent } from './recipe/feature/recipes/recipes.component';
 export const routes: Routes = [
 	{ path: '', redirectTo: 'home', pathMatch: 'full' },
 	{ path: 'home', component: RecipesComponent },
-	{ path: 'receitas/:id', component: RecipeComponent },
+	{
+		path: 'receitas/:id',
+		loadComponent: () =>
+			import('./recipe/feature/recipe/recipe.component').then(
+				(c) => c.RecipeComponent
+			),
+	},
 	{ path: '**', redirectTo: 'home' },
 ];
