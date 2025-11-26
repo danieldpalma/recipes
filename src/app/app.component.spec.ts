317bc0cc2b6b737ed2ff9d731e4445314bd14a0c
danieldpalma <danielpalma232@gmail.com>
2025-11-26 14:10:44 -0300

    test app component implemented

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,26 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+	let component: AppComponent;
+	let fixture: ComponentFixture<AppComponent>;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [AppComponent],
+			providers: [provideRouter([])],
+		}).compileComponents();
 
-  it(`should have the 'recipes' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('recipes');
-  });
+		fixture = TestBed.createComponent(AppComponent);
+		component = fixture.componentInstance;
+	});
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, recipes');
-  });
+	it('should create the app', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it(`should have the 'My Recipe Book' title`, () => {
+		expect(component.title).toEqual('My Recipe Book');
+	});
 });
